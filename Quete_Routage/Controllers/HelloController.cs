913812cc3735c2979394745a03c566e2fb541616
Sebastien David <sebastien.david08@gmail.com>
2021-11-02 19:12:43 +0100

    Ajoutez des fichiers projet.

---
+++ Quete_Routage/Controllers/HelloController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quete_Routage.Controllers
+{
+    public class HelloController : Controller
+    {
+        public String SayHello()
+        {
+            return "Hello !";
+        }
+
+        public String YellHello()
+        {
+            return "HELLO !";
+        }
+
+        [Route("/Say")] // The endpoint here is reached by calling `/Say` route
+        public String SayHelloAttribut()
+        {
+            return "Hello !";
+        }
+
+        [Route("/Yell")] // The endpoint here is reached by calling `/Yell` route
+        public String YellHelloAttribut()
+        {
+            return "HELLO !";
+        }
+    }
+}
---
+++ Quete_Routage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Quete_Routage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
